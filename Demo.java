158c90659b655fb798a893bf08e0dd6f8d3747b4
Stevodeveloper <stepheneoduor20@gmail.com>
2024-06-24 10:12:32 +0300

    prctice on packages

---
+++ Demo.java
@@ -0,0 +1,12 @@
+
+import testpack.MyName;
+
+public class Demo {
+    public static void main(String[] args) {
+        MyName name = new MyName();
+
+        String jina = "Stephen Oduor Otieno";
+
+        name.getName(jina);
+    }
+}
---
+++ testpack/MyName.java
@@ -0,0 +1,8 @@
+package testpack;
+
+
+public class MyName {
+    public void getName(String name){
+        System.out.println("my name is: " + name);
+    }
+}
